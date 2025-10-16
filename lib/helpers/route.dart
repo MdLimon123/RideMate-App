a22945f2f6ef8edc0529557e1ade0bef6d8a14c0
Md Limon Islam <limon222019@gmail.com>
2025-10-16 11:19:29 +0600

    user sign up done

--- lib/helpers/route.dart
+++ lib/helpers/route.dart
@@ -3,7 +3,9 @@ import 'package:radeef/views/screen/Home/home_screen.dart';
 import 'package:radeef/views/screen/Profile/profile_screen.dart';
 import 'package:radeef/views/screen/Splash/select_role_screen.dart';
 import 'package:radeef/views/screen/UserFLow/UserAuth/select_language_screen.dart';
+import 'package:radeef/views/screen/UserFLow/UserAuth/terms_condition_screen.dart';
 import 'package:radeef/views/screen/UserFLow/UserAuth/user_login_screen.dart';
+import 'package:radeef/views/screen/UserFLow/UserAuth/user_signup_screen.dart';
 import 'package:radeef/views/screen/Wallet/wallet_screen.dart';
 import '../views/screen/Splash/splash_screen.dart';
 
@@ -16,6 +18,10 @@ class AppRoutes{
   static String walletScreen="/wallet_screen";
   static String selectUserLanguage = "/select_user_language";
   static String userLoginScreen = "/user_login_screen";
+  static String userSignUpScreen = "/user_sign_up_screen";
+  static String userForgotPasswordScreen = "/user_forgot_password_screen";
+  static String userTermsConditionScreen = "/user_terms_condition_screen";
+
 
 
  static List<GetPage> page=[
@@ -26,6 +32,8 @@ class AppRoutes{
    GetPage(name:selectRole, page: ()=>const SelectRoleScreen(),transition: Transition.noTransition),
    GetPage(name:selectUserLanguage, page: ()=>const SelectUserLanguageScreen(),transition: Transition.noTransition),
    GetPage(name:userLoginScreen, page: ()=>const UserLoginScreen(),transition: Transition.noTransition),
+   GetPage(name:userSignUpScreen, page: ()=>const UserSignupScreen(),transition: Transition.noTransition),
+   GetPage(name:userTermsConditionScreen, page: ()=>const TermsConditionScreen(),transition: Transition.noTransition),
   ];
 
 
--- lib/views/screen/UserFLow/UserAuth/user_signup_screen.dart
+++ lib/views/screen/UserFLow/UserAuth/user_signup_screen.dart
@@ -6,17 +6,18 @@ import 'package:radeef/utils/app_colors.dart';
 import 'package:radeef/views/base/custom_button.dart';
 import 'package:radeef/views/base/custom_text_field.dart';
 
-class UserLoginScreen extends StatefulWidget {
-  const UserLoginScreen({super.key});
+class UserSignupScreen extends StatefulWidget {
+  const UserSignupScreen({super.key});
 
   @override
-  State<UserLoginScreen> createState() => _UserLoginScreenState();
+  State<UserSignupScreen> createState() => _UserSignupScreenState();
 }
 
-class _UserLoginScreenState extends State<UserLoginScreen> {
-
+class _UserSignupScreenState extends State<UserSignupScreen> {
   final emailTextController = TextEditingController();
   final passwordTextController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -25,51 +26,47 @@ class _UserLoginScreenState extends State<UserLoginScreen> {
         padding: EdgeInsets.symmetric(horizontal: 20),
         children: [
           SizedBox(height: 100,),
-          Text("Log In",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w500,
-            color: AppColors.textColor
-          ),),
+          Text("Sign Up",
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+                color: AppColors.textColor
+            ),),
           SizedBox(height: 24,),
           CustomTextField(controller: emailTextController,
-          isEmail: true,
-          hintText: "Enter your email",),
+            isEmail: true,
+            hintText: "Enter your email",),
           SizedBox(height: 12,),
           CustomTextField(controller: passwordTextController,
-          hintText: "Enter Password",),
+            hintText: "Enter Password",),
           SizedBox(height: 12,),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Text("Forget Password?",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
-              color: Color(0xFF345983)
-            ),),
-          ),
+          CustomTextField(controller: confirmPasswordController,
+            hintText: "Confirm Password",),
+
           SizedBox(height: 293,),
-          CustomButton(onTap: (){},
-              text: "Log In"),
+          CustomButton(onTap: (){
+            Get.offAllNamed(AppRoutes.userTermsConditionScreen);
+          },
+              text: "Sign Up"),
           SizedBox(height: 20,),
           Center(
             child: RichText(
               text: TextSpan(
-                text: "Don’t have account? ",
+                text: "Already have an account? ",
                 style: TextStyle(
                     color: Color(0xFF5A5A5A),
                     fontSize: 16,
                     fontWeight: FontWeight.w400),
                 children: [
                   TextSpan(
-                    text: "Sign Up",
+                    text: "Log In",
                     style: TextStyle(
                         color: Color(0xFF145788),
                         fontSize: 16,
                         fontWeight: FontWeight.w400),
                     recognizer: TapGestureRecognizer()
                       ..onTap = () {
-                      Get.offAllNamed(AppRoutes.userSignUpScreen);
+                      Get.offAllNamed(AppRoutes.userLoginScreen);
                       },
                   ),
                 ],
