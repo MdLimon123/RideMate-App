46fd3f17522ed247a049745b6adc6154fb2dd0ef
Wasiul Islam Sakif <sakif049@gmail.com>
2026-02-03 15:20:45 +0600

    refactor: improve DriverForgetPasswordScreen structure and add loading state

--- lib/views/screen/DriverFlow/DriverAuth/driver_forget_password_screen.dart
+++ lib/views/screen/DriverFlow/DriverAuth/driver_forget_password_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:radeef/controllers/DriverController/driver_auth_controller.dart';
 import 'package:radeef/helpers/route.dart';
 import 'package:radeef/utils/app_colors.dart';
 import 'package:radeef/views/base/custom_button.dart';
@@ -9,12 +10,14 @@ class DriverForgetPasswordScreen extends StatefulWidget {
   const DriverForgetPasswordScreen({super.key});
 
   @override
-  State<DriverForgetPasswordScreen> createState() => _DriverForgetPasswordScreenState();
+  State<DriverForgetPasswordScreen> createState() =>
+      _DriverForgetPasswordScreenState();
 }
 
-class _DriverForgetPasswordScreenState extends State<DriverForgetPasswordScreen> {
-
+class _DriverForgetPasswordScreenState
+    extends State<DriverForgetPasswordScreen> {
   final emailTextController = TextEditingController();
+  final _driverAuthController = Get.put(DriverAuthController());
 
   @override
   Widget build(BuildContext context) {
@@ -22,22 +25,33 @@ class _DriverForgetPasswordScreenState extends State<DriverForgetPasswordScreen>
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 20),
         children: [
-          SizedBox(height: 100,),
-          Text("forget".tr,
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.w500,
-            color: AppColors.textColor
-          ),),
-          SizedBox(height: 32,),
-          CustomTextField(controller: emailTextController,
+          SizedBox(height: 100),
+          Text(
+            "forget".tr,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w500,
+              color: AppColors.textColor,
+            ),
+          ),
+          SizedBox(height: 32),
+          CustomTextField(
+            controller: emailTextController,
             isEmail: true,
-            hintText: "Enter your email",),
-          SizedBox(height: 404,),
-          CustomButton(onTap: (){
-            Get.offAllNamed(AppRoutes.driverOtpVerifyScreen);
-          },
-              text: "sendOTP".tr)
+            hintText: "Enter your email",
+          ),
+          SizedBox(height: 404),
+          Obx(
+            () => CustomButton(
+              loading: _driverAuthController.isForgetLoading.value,
+              onTap: () {
+                _driverAuthController.forgetPassword(
+                  email: emailTextController.text,
+                );
+              },
+              text: "sendOTP".tr,
+            ),
+          ),
         ],
       ),
     );
